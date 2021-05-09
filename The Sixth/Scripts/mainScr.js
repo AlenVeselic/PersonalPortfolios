0a4196d47a740b279ec8793d95c560a517a739b3
Alen Veselič <68716124+AlenVeselic@users.noreply.github.com>
2021-05-09 20:05:55 +0200

    Cleaned up content and made the layout more responsive

--- The Sixth/Scripts/mainScr.js
+++ The Sixth/Scripts/mainScr.js
@@ -1,8 +1,13 @@
-const matches = document.querySelectorAll("div[class] div[id]")
+const matches = document.querySelectorAll(".fade")
 let i = 0
 for(match of matches){
+    match.style["opacity"] = "0%"
+    match.style["animation"] = "fadeIn 1s forwards"
     match.style["animationDelay"] = i.toString() + "s"
     i = i+1
+    if(i == 7){
+        break
+    }
 }
 
 function toggleDisplay(){
@@ -12,3 +17,35 @@ function toggleDisplay(){
 
 }
 
+let lastKnownScrollPosition = 0;
+
+document.addEventListener('scroll', function(e) {
+    lastKnownScrollPosition = window.scrollY;
+    
+    document.getElementsByClassName("scrollSee")[0].innerHTML = lastKnownScrollPosition
+
+    if(Math.round(scrollY) >= 200){
+    matches[7].style["opacity"] = "0%"
+    matches[7].style["animation"] = "fadeIn 1s forwards"
+    matches[7].style["animationDelay"] = "1s"
+    }
+    
+    if(Math.round(scrollY) >= 1400){
+    matches[8].style["opacity"] = "0%"
+    matches[8].style["animation"] = "fadeIn 1s forwards"
+    matches[8].style["animationDelay"] = "1s"
+    }
+
+    if(Math.round(scrollY) >= 2000){
+    matches[9].style["opacity"] = "0%"
+    matches[9].style["animation"] = "fadeIn 1s forwards"
+    matches[9].style["animationDelay"] = "1s"
+    }
+
+    if(Math.round(scrollY) >= 2200){
+        matches[10].style["opacity"] = "0%"
+    matches[10].style["animation"] = "fadeIn 1s forwards"
+    matches[10].style["animationDelay"] = "1s"
+    }
+
+    });
