4656cdfbe0bbaea8c81ba2734dde2c02d2f8bceb
Alen Veselič <68716124+AlenVeselic@users.noreply.github.com>
2021-05-10 11:18:06 +0200

    More responsivenes and visual work
    The projects and concepts sections now turn their items into a column on smaller screens.
    
    Made the fade in assign all info to appear on a set timer if user is at the bottom of the page on refresh.
    
    Otherwise the info appears as the person scrolls down the page a section at a time.

--- The Sixth/Scripts/mainScr.js
+++ The Sixth/Scripts/mainScr.js
@@ -18,13 +18,41 @@ function toggleDisplay(){
 }
 
 let lastKnownScrollPosition = 0;
+let sectionPixelRanges = [
+    200, 1400, 2000, 2100
+]
+let sectionEnumerator = 7
 
 document.addEventListener('scroll', function(e) {
     lastKnownScrollPosition = window.scrollY;
     
     document.getElementsByClassName("scrollSee")[0].innerHTML = lastKnownScrollPosition
 
-    if(Math.round(scrollY) >= 200){
+    
+    if(Math.round(scrollY) >= sectionPixelRanges[sectionPixelRanges.length - 1]){
+        let timeDelay = i
+         while(sectionEnumerator != 11){
+             sectionPixelRanges.shift()
+             matches[sectionEnumerator].style["animation"] = "fadeIn 1s forwards"
+             matches[sectionEnumerator].style["animationDelay"] = timeDelay.toString() + "s"
+             sectionEnumerator++;
+             timeDelay++;
+         }
+     }
+    
+
+        if(Math.round(scrollY) >= sectionPixelRanges[0]){
+            sectionPixelRanges.shift()
+            matches[sectionEnumerator].style["animation"] = "fadeIn 1s forwards"
+            matches[sectionEnumerator].style["animationDelay"] = "1s"
+            sectionEnumerator++;
+        }
+
+        
+
+    
+
+    /*if(Math.round(scrollY) >= 200){
     matches[7].style["opacity"] = "0%"
     matches[7].style["animation"] = "fadeIn 1s forwards"
     matches[7].style["animationDelay"] = "1s"
@@ -47,5 +75,6 @@ document.addEventListener('scroll', function(e) {
     matches[10].style["animation"] = "fadeIn 1s forwards"
     matches[10].style["animationDelay"] = "1s"
     }
-
+    */
     });
+
